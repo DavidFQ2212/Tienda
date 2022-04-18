268c5ab9ea86f6c13f028c5a8d83a52ad8e50a90
DavidFQ2212 <davidqc09@hotmail.com>
2022-04-17 18:27:35 -0600

    creacion

---
+++ src/main/java/com/Tienda/Tienda/modelos/ModeloUsuario.java
@@ -0,0 +1,40 @@
+public class ModeloUsuario{
+    private long id;
+    private String nombre;
+    private String email;
+    private int Prioridad;
+
+
+
+public void setId(long id){
+    this.id=id;
+
+}
+public long getI(){
+    return id;
+}
+public void setNombre(String nombre){
+    this.nombre=nombre;
+}
+public String getNombrre(){
+    return nombre;
+}
+public void setEmail(String email){
+    this.email=email;
+}
+public String getEmail(){
+    return email;
+}
+public void setPrioridad(String prioridad){
+    this.prioridad=prioridad;
+}
+public String getPrioridad(){
+    return prioridad;
+}
+
+
+
+
+
+
+}
